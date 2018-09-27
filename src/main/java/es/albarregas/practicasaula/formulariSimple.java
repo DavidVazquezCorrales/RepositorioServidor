dfee263626394a19ded925f9e470b975995b9598
David <David@David-PC>
2018-09-27 19:14:39 +0200

    COMMIT desde Netbeans

---
+++ src/main/java/es/albarregas/practicasaula/formulariSimple.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.practicasaula;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author David
+ */
+@WebServlet(name = "formulariSimple", urlPatterns = {"/formulariSimple"})
+public class formulariSimple extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response, boolean correcto)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet formulariSimple</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+           String nombre = null;
+           
+            
+            if(!correcto){
+            out.println("<p>No se han introducido datos</p>");
+            }else{
+                Enumeration<String> parametros=request.getParameterNames();
+                while(parametros.hasMoreElements())
+                    nombre=parametros.nextElement();
+                    if(!nombre.startsWith("enviar")){
+                         out.println(nombre+" -<strong>"+request.getParameter(nombre)+"</strong><br/>");
+                         
+                }
+            
+            }
+            out.println("<p><a href='"+request.getContextPath()+"'>Men&uacet;inicial</a></p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        boolean correcto=true;
+        
+        processRequest(request, response,correcto);
+    
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       boolean correcto=false;
+    
+        processRequest(request, response,correcto);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
